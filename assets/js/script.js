84154c091f98b7461c9e768602e0997d75d6964e
Carlos <c.hickmann86@gmail.com>
2019-05-14 22:19:53 -0300

    footer y redes sociales

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,20 @@ $(function() {
     $('#myModal').on('shown.bs.modal', function() {
         $('#myInput').trigger('focus')
     })
+
+    $("a").click(function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var scroll = this.hash;
+
+            $("html, body").animate({
+                scrollTop: $(scroll).offset().top
+            }, 600, function() {
+                window.location.hash = scroll;
+            });
+        }
+    });
+
+    $('[data-toggle="popover"]').popover();
 });
